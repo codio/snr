045bc318377929a5fff3cbfcc7ba786a8d3ffe6d
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2013-10-18 00:47:58 +0200

    Make much more efficient

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-var Glob = require('glob').Glob;
+var glob = require('glob');
 var spawn = require('child_process').spawn;
 
 // Usage
@@ -9,31 +9,19 @@ var spawn = require('child_process').spawn;
 
 var args = process.argv.slice(2);
 
-
-
-var searchArgs = args.shift().split(' ');
+var defaultArgs = args.shift().split(' ').concat(['-C', '2']);
 
 var paths = args;
 
-
 paths.forEach(function(location) {
-  var matcher = new Glob(location);
-
-  matcher.on('match', ag);
-
-  matcher.on('error', function (err) {
-    console.error(err);
+  glob(location, function (error, files) {
+    if (error) return console.error(error);
+    ag(files);
   });
 });
 
-// Execute ag on a given file
-function ag(file) {
-  var child = spawn('ag', searchArgs.concat([file, '--nocolor', '-C', '2']));
-  var output = [];
-  child.stdout.on('data', function (d) {output.push(d)});
-  child.stdout.on('end', function () {
-    if (output.length > 0) {
-      console.log([file, '\n'].concat(output).join(''));
-    }
-  });
+// Execute ag on a list of files
+function ag(files) {
+  console.log(defaultArgs.concat(files));
+  var child = spawn('ag', defaultArgs.concat(files), {customFds: [0, 1, 2]});
 }
