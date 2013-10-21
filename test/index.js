bf624f7be2e69e1dab08fb5c1e04de7d9f8bb05a
Friedel Ziegelmayer <dignifiedquire@gmail.com>
2013-10-21 16:13:50 +0200

    tests(replace): Whole word option.

--- test/index.js
+++ test/index.js
@@ -1,14 +1,16 @@
 require('better-stack-traces').install();
-var fs = require('fs');
 var stream = require('stream');
 
 var expect = require('chai').expect;
 var sinon = require('sinon');
 var SandboxedModule = require('sandboxed-module');
 var strs = require('stringstream');
+var sh = require('execSync');
+var fs = require('fs-extra');
 
 
 var childProcess = require('child_process');
+var ack = sh.run('hash ack-grep 2>/dev/null') === 1 ? 'ack' : 'ack-grep';
 
 // Fixture files
 var simpleResult = fs.readFileSync(__dirname + '/fixtures/simple.txt').toString();
@@ -67,12 +69,46 @@ describe('search and replace', function () {
   });
 
   describe('replace', function () {
-    beforeEach(function () {
+    // var exec = sinon.stub(childProcess, 'exec');
+    // var glob = sinon.stub();
+    var destPath = __dirname + '/fixtures/simpleReplace.txt';
+
+    beforeEach(function (done) {
       replace = require('../index').replace;
+
+      // Copy replace file
+      var origPath = __dirname + '/fixtures/simpleOriginal.txt';
+      fs.copy(origPath, destPath, done);
+    });
+
+    afterEach(function (done) {
+      fs.remove(destPath, done);
     });
 
     it('should exist', function () {
       expect(replace).to.be.a('function');
+    });
+
+    describe('options', function () {
+      describe('-w whole word', function () {
+        it('should work with literal option', function (done) {
+          var opts = {
+            replace: 'codio',
+            literal: true,
+            wordRegexp: true,
+            ignoreCase: true,
+            cmd: ack
+          };
+          var out = replace(destPath, 'team', opts);
+          out.on('data', function () {});
+          out.on('end', function () {
+            var result = fs.readFileSync(destPath).toString();
+            var expected = fs.readFileSync(__dirname + '/fixtures/simpleExpected.txt').toString();
+            expect(result).to.be.eql(expected);
+            done();
+          });
+        });
+      });
     });
   });
 });
